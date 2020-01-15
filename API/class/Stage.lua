7cc32ae4e110a8748450805414080f874c184e30
Uziskull <jtunes_9@hotmail.com>
2020-01-15 14:03:08 +0000

    fixed typo

--- API/class/Stage.lua
+++ API/class/Stage.lua
@@ -234,7 +234,7 @@ do
 		stage_origin[new] = context
 		stage_subname[new] = ""
 		stage_list_interactable[new] = dsWrapper.list(interactable_list, "Interactable", RoRInteractable)
-		stage_map_interactable_rarity[new] = dsWrapper.map(AnyTypeRetrarity_map, "Interactable", RoRInteractable, nil, "number", nil, nil)
+		stage_map_interactable_rarity[new] = dsWrapper.map(rarity_map, "Interactable", RoRInteractable, nil, "number", nil, nil)
 		stage_list_enemy[new] = dsWrapper.list(enemy_list, "MonsterCard", RoRMonsterCard)
 		stage_list_rooms[new] = dsWrapper.list(room_list, "Room", GMRoom, nil, stageRoomAdded, stageRoomRemoved)
 
