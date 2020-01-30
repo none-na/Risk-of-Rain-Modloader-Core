7ae1ba59eda3ed291b299a3621a4cfe3c137047a
none <nonena@protonmail.com>
2020-01-30 22:48:52 +0300

    Some code that doesn't work and team checking for damage method

--- API/class/object/ProjectileInstance.lua
+++ API/class/object/ProjectileInstance.lua
@@ -12,23 +12,27 @@ GMInstance.ProjectileInstance = static
 
 do
 	-- Reduces argument count, handles knockback direction and specific_target
+	-- The noteam argument is there since specific_target overrides team
 	local DISTANCE = 16
-	function lookup:damage(instance, damage, properties)
+	function lookup:damage(instance, damage, properties, noteam)
 		if not childeren[self] then methodCallError("ProjectileInstance:damage", self) end
 		if not isA(instance, "ActorInstance") then typeCheckError("ProjectileInstance:damage", 1, "instance", "ActorInstance", instance) end
 		if type(damage) ~= "number"           then typeCheckError("ProjectileInstance:damage", 2, "damage",   "number",          damage) end
 		if type(properties) ~= "number" and properties ~= nil then typeCheckError("ProjectileInstance:damage", 3, "properties", "number or nil", properties) end
+		if type(noteam) ~= "boolean" and noteam ~= nil then typeCheckError("ProjectileInstance:damage", 4, "noteam", "boolean or nil", noteam) end
 		
-		local direction = math.sign(instance.x - self.x)
-		return self:fireBullet(
-			instance.x - direction * DISTANCE,
-			instance.y,
-			90 * (1 - direction),
-			DISTANCE,
-			damage,
-			nil,
-			properties
-		):set("specific_target", instance.id)
+		if noteam or (instance:get("team") ~= self:get("team")) then
+			local direction = math.sign(instance.x - self.x)
+			return self:fireBullet(
+				instance.x - direction * DISTANCE,
+				instance.y,
+				90 * (1 - direction),
+				DISTANCE,
+				damage,
+				nil,
+				properties
+			):set("specific_target", instance.id)
+		end
 	end
 	
 	-- Fire functions
@@ -130,4 +134,71 @@ do
 		local t = AnyTypeRet(GML.variable_instance_get(ids[self], "parent"))
 		return ((t >= 0) and iwrap(t) or nil)
 	end
+	
+	-- Doesn't work currently
+	--[[
+	-- Makes the projectile stop (or bounce if set) before colliding with the map
+	-- Should be used on the map entry or collide callback
+	function lookup:moveSolid(bounce)
+		if type(bounce) ~= "number" and bounce ~= nil then typeCheckError("ProjectileInstance:moveSolid", 1, "bounce", "number or nil", bounce) end
+		
+		local bounce = bounce or 0
+		
+		-- Enters pre-collision state
+		local hs, vs = self:get("hspeed"), self:get("vspeed")
+		self.x = self.x - hs
+		self.y = self.y - vs
+
+		-- Moves in pixels or below on each axis until collision occurs and stops the projectile
+		local dx, dy = hs, vs
+		local sx, sy = math.sign(dx), math.sign(dy)
+		repeat
+			-- Get distance to travel at most a pixel
+			local step_dx, step_dy = sx * math.min(math.abs(dx), 1), sy * math.min(math.abs(dy), 1)
+			
+			if not self:collidesMap(self.x + step_dx, self.y + step_dy) then
+				self.x = self.x + step_dx
+				self.y = self.y + step_dy
+				dx = dx - step_dx
+				dy = dy - step_dy
+			else
+				local h_collision = self:collidesMap(self.x + step_dx, self.y)
+				local v_collision = self:collidesMap(self.x, self.y + step_dy)
+				local h_stop, v_stop
+				
+				-- Horizontal
+				if h_collision then
+					h_stop = true
+				end
+				
+				-- Vertical
+				if v_collision then
+					v_stop = true
+				end
+				
+				-- Corner (both horizontal and vertical)
+				if (not h_collision) and (not v_collision) then
+					h_stop = true
+					v_stop = true
+				end
+				
+				if h_stop then
+					dx = 0
+					--self:set("hspeed", - self:get("haccel") - hs * bounce)
+					self:set("hspeed", - self:get("haccel"))
+				end
+				
+				if v_stop then
+					dy = 0
+					--self:set("vspeed", - self:get("vaccel") - vs * bounce)
+					self:set("vspeed", - self:get("vaccel"))
+				end
+			end
+			
+			-- The sign becomes 0 or becomes the negative of what it originally was
+			if math.sign(dx) ~= sx then dx = 0 end
+			if math.sign(dy) ~= sy then dy = 0 end
+		until (dx == 0) and (dy == 0)
+	end
+	--]]
 end
